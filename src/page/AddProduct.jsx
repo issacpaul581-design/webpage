7a3c2ba4d45b3563c87cadb49d21ff7e261a9e5c
issacpaul581-design <issacpaul582@gmail.com>
2026-06-19 13:27:55 +0530

    Fixxx

--- src/page/AddProduct.jsx
+++ src/page/AddProduct.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import "./addProduct.css"
+import "./addproduct.css"
 
 function AddProduct() {
   const navigate = useNavigate()
